c17b320d206dcfe98d613ed66b983267cce971f1
Ujify1337 <rd14001@gmail.com>
2023-10-19 15:26:49 +0100

    changes”
    git push -u origin master
    git commit -m changes”

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://b64500e2-cc80-4ca6-aa06-a84e302841e6.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK = "/api/v1/orders/track"
